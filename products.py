31169b33f431e17ba34aa1fed4c3beef926d822c
sudomango <mark.saburov.work@gmail.com>
2023-11-03 20:20:09 +0000

    Добавлены дочерние классы для категорий товаров

--- products.py
+++ products.py
@@ -1,7 +1,10 @@
 import os
 from datetime import datetime
+from json_worker import JsonWorker
 
 class Product:
+    init_values = JsonWorker().unpack(os.path.dirname(__file__) + "/json/init_values.json")
+    products_prices = JsonWorker().unpack(os.path.dirname(__file__) + "/json/purchase_price.json")
 
     def __init__(self, product_class, products_amount, date):
         pass
@@ -23,3 +26,28 @@ class Product:
 
     def is_fresh(self, today):
         pass
+
+
+class Fruits(Product):
+    def __init__(self, products_amount, date):
+        super().__init__("Fruits", products_amount, date)
+
+
+class Drinks(Product):
+    def __init__(self, products_amount, date):
+        super().__init__("Drinks", products_amount, date)
+
+
+class Books(Product):
+    def __init__(self, products_amount, date):
+        super().__init__("Books", products_amount, date)
+
+
+class HouseGoods(Product):
+    def __init__(self, products_amount, date):
+        super().__init__("Household Goods", products_amount, date)
+
+
+class QuickBreakfasts(Product):
+    def __init__(self, products_amount, date):
+        super().__init__("Quick Breakfasts", products_amount, date)
