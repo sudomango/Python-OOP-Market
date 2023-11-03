3050a38cfd79fd0a0466438c97822be49cc03dc6
sudomango <mark.saburov.work@gmail.com>
2023-11-03 19:55:37 +0000

    Добавлены новые атрибуты и методы для класса CurrentDate

--- current_date.py
+++ current_date.py
@@ -3,16 +3,26 @@ from datetime import datetime, timedelta
 class CurrentDate:
 
     def __init__(self):
-        pass
+        self.__date = datetime.today().date()
 
-    def add_days(self, number_of_days):
-        pass
+    def add_days(self, number_of_days: int) -> bool:
+        if number_of_days >=0 and number_of_days <= 3000:
+            self.__date += timedelta(days = number_of_days)
+            return True
+        else:
+            print("Ошибка! Количество дней указано неверно. Укажите целое число в диапазоне 0 .. 3000.")
+            return False
 
-    def sub_days(self, number_of_days):
-        pass
+    def sub_days(self, number_of_days: int) -> bool:
+        if number_of_days >=0 and number_of_days <= 3000:
+            self.__date -= timedelta(days = number_of_days)
+            return True
+        else:
+            print("Ошибка! Количество дней указано неверно. Укажите целое число в диапазоне 0 .. 3000.")
+            return False
 
     def get_date(self):
-        pass
+        return self.__date
 
     def print_date(self):
-        pass
+        print(self.__date.strftime("%d.%m.%Y"))
