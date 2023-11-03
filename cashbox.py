a3713a86da55a9f7377e46f98b33fd9ce419b674
sudomango <mark.saburov.work@gmail.com>
2023-11-03 19:48:15 +0000

    Добавлены новые атрибуты и методы для класса Cashbox

--- cashbox.py
+++ cashbox.py
@@ -1,13 +1,24 @@
 class Cashbox:
 
-    def __init__(self, model):
-        pass
+    def __init__(self, model: str = "Model MK-03"):
+        self.__model = model
+        self.__balance = 0
 
-    def add_cash(self, amount):
-        pass
+    def add_cash(self, amount: float) -> bool:
+        if amount >= 0:
+            self.__balance += amount
+            return True
+        else:
+            print("Ошибка! Вы ввели неверную сумму.")
+            return False
 
-    def sub_cash(self, amount):
-        pass
+    def sub_cash(self, amount: float) -> bool:
+        if amount >= 0 and self.__balance >= amount:
+            self.__balance -= amount
+            return True
+        else:
+            print("Ошибка! Вы ввели неверную сумму или в кассе недостаточно средств.")
+            return False
 
     def get_balance(self):
-        pass
+        return self.__balance
