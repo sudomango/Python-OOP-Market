249707d332fb1085b00c8c4cd426dcfa2790cb1e
sudomango <mark.saburov.work@gmail.com>
2023-11-03 19:02:27 +0000

    Спроектирован класс CurrentDate для управления текущей датой

---
+++ current_date.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timedelta
+
+class CurrentDate:
+
+    def __init__(self):
+        pass
+
+    def add_days(self, number_of_days):
+        pass
+
+    def sub_days(self, number_of_days):
+        pass
+
+    def get_date(self):
+        pass
+
+    def print_date(self):
+        pass
