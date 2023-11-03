f96323782320bd2f6b23a0d77e6f4a1b122bbbd6
sudomango <mark.saburov.work@gmail.com>
2023-11-03 22:04:11 +0000

    Добавлены методы по распечатке прайс-листа и содержимого склада

--- market.py
+++ market.py
@@ -37,9 +37,51 @@ class Market:
         pass
 
     # Распечатать содержимое склада (какие товары сейчас на нём хранятся и в каком количестве).
+    # Можно распечатать прямо в окно терминала, а можно в json-файл (экспериментально).
     def print_storage_content(self, destination: str = "terminal") -> bool:
-        pass
+
+        if destination == "terminal":
+            products = self.__storage.get_storage_info()
+            print()
+            if products:
+                print("На складе сейчас хранится:\n")
+                for prod_key in products.keys():
+                    print(f"{prod_key} = {products[prod_key]} ед.")
+            else:
+                print("На складе сейчас пусто. Совершите закупку товара.")
+            return True
+
+        elif destination == "json":
+            if not Path("./json_output").exists(): Path("./json_output").mkdir(True, True)
+            products = self.__storage.get_storage_info()
+            JsonWorker().pack("./json_output/storage_content.json", products)
+            print("Файл был успешно записан. Вы можете найти его по адресу ./json_output/storage_content.json")
+            return True
+
+        else:
+            print("Ошибка! Вы неверно указали объект назначения для печати. Выберите \"terminal\" или \"json\".")
+            return False
 
     # Распечатать текущий прайс-лист на товары (печать также возможна либо в окно терминала, либо в json-файл).
+    # Примечание: Распечатываются только те товары, которые есть в наличии на складе (кол-во единиц товара > 0).
     def print_prices(self, destination: str = "terminal") -> bool:
-        pass
+
+        if destination == "terminal":
+            products = self.__storage.get_storage_info()
+            print()
+            for key in products.keys():
+                if products[key] > 0:
+                    selling_price = self.__product_class_to_object(key).get_selling_price()
+                    print(f"Стоимость продажи для категории товара {key} = {selling_price}")
+        elif destination == "json":
+            if not Path("./json_output").exists(): Path("./json_output").mkdir(True, True)
+            market_prices = {}
+            for key in products.keys():
+                if products[key] > 0:
+                    market_prices[key] = self.__product_class_to_object(key).get_selling_price()
+            JsonWorker().pack("./json_output/market_prices.json", market_prices)
+            print("Файл был успешно записан. Вы можете найти его по адресу ./json_output/market_prices.json")
+            return True
+        else:
+            print("Ошибка! Вы неверно указали объект назначения для печати. Выберите \"terminal\" или \"json\".")
+            return False
