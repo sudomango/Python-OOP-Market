6cac114b4be7c597e37417ea2dde1385be400fd9
sudomango <mark.saburov.work@gmail.com>
2023-11-03 19:01:04 +0000

    Спроектирован шаблон для класса Cashbox (Касса)

---
+++ cashbox.py
@@ -0,0 +1,13 @@
+class Cashbox:
+
+    def __init__(self, model):
+        pass
+
+    def add_cash(self, amount):
+        pass
+
+    def sub_cash(self, amount):
+        pass
+
+    def get_balance(self):
+        pass
