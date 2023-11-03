9b2c88dd36477b1159f89e6a0fddbc0ae8900af7
sudomango <mark.saburov.work@gmail.com>
2023-11-03 19:32:08 +0000

    Спроектирован шаблон для класса Product (Товар)

---
+++ products.py
@@ -0,0 +1,25 @@
+import os
+from datetime import datetime
+
+class Product:
+
+    def __init__(self, product_class, products_amount, date):
+        pass
+
+    def get_manufacture_date(self):
+        pass
+
+    def get_purchase_price(self):
+        pass
+
+    def get_amount(self):
+        pass
+
+    def set_amount(self, new_amount):
+        pass
+
+    def get_selling_price(self):
+        pass
+
+    def is_fresh(self, today):
+        pass
