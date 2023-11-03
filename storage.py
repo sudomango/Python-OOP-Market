e78d6e8561499c635a9178208ecd5075884726b3
sudomango <mark.saburov.work@gmail.com>
2023-11-03 19:19:35 +0000

    Спроектирован шаблон для класса Storage (Склад)

---
+++ storage.py
@@ -0,0 +1,25 @@
+from random import randint
+from current_date import CurrentDate
+
+class Storage:
+
+    def __init__(self):
+        pass
+
+    def save_to_storage(self, product_class, products_amount):
+        pass
+
+    def get_from_storage(self, product_class, products_amount):
+        pass
+
+    def get_storage_info(self):
+        pass
+
+    def get_storage_content(self):
+        pass
+
+    def get_capacity_info(self):
+        pass
+
+    def get_free_place_info(self):
+        pass
