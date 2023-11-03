a80891eeadebf764c985f5ce25872a11a94d1c13
sudomango <mark.saburov.work@gmail.com>
2023-11-03 19:33:40 +0000

    Спроектирован шаблон для класса Market (Супермаркет)

---
+++ market.py
@@ -0,0 +1,29 @@
+from random import randint
+
+class Market:
+    def __init__(self, start_cash = 200):
+        pass
+
+    def make_order(self, product_class, amount):
+        pass
+    
+    def sell_products(self, product_class, amount):
+        pass
+
+    def check_cashbox(self):
+        pass
+
+    def check_storage(self):
+        pass
+    
+    def count_expire_goods(self):
+        pass
+    
+    def print_expire_goods(self):
+        pass
+
+    def print_prices(self, destination):
+        pass
+
+    def print_storage_content(self, destination):
+        pass
