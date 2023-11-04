8231544a948ef182582dcfd0611c732b598c8e70
sudomango <mark.saburov.work@gmail.com>
2023-11-04 09:15:08 +0000

    Добавлены комментарии для методов класса Storage

--- market_storage.py
+++ market_storage.py
@@ -11,12 +11,15 @@ class Storage:
         self.__products_amount = {}
         self.__free_place = self.__capacity
 
+    # Сохранить товары указанного класса в указанном кол-ве на складе Маркета (если хватает свободного места).
     def save_to_storage(self, product_class: str, products_amount: int) -> bool:
         pass
 
+    # Забрать товары указанного класса в указанном кол-ве со склада (если они, конечно, имеются в наличии).
     def get_from_storage(self, product_class: str, products_amount: int) -> bool:
         pass
 
+    # Вспомогательный метод, позволяющий увеличить вместительность склада.
     def __increase_capacity(self, n: int):
         if n >= 0:
             self.__capacity += n
@@ -25,14 +28,18 @@ class Storage:
         else:
             return False
 
+    # Возвращает содержимое склада в виде словаря {product_class: products_amount, ...}.
     def get_storage_info(self):
         return self.__products_amount
 
+    # Возвращает содержимое склада в виде словаря {product_class: [list of Product Objects]}.
     def get_storage_content(self):
         return self.__storage
 
+    # Возвращает количество свободного места на складе (в момент вызова метода).
     def get_free_place_info(self):
         return self.__free_place
 
+    # Возвращает кортеж вида (свободное_место, общая_вместимость_склада).
     def get_capacity_info(self):
         return (self.__free_place, self.__capacity)
